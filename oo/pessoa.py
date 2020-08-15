9407c593122d392dd5f0366914865449554593bc
Lucas de Moura Quadros <lucasmq92@gmail.com>
2020-08-15 19:51:42 -0300

    Criando atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,19 @@
 class Pessoa:
-    pass
+    def __init__(self,nome=None,idade=27):
+        self.nome = nome
+        self.idade = idade
+    def cumprimentar(self):
+        return f'Olá id{self}'
+
+if __name__ == '__main__':
+    p = Pessoa('Lucas')
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())     #implicitamente, passa p como parâmetro, assim como qualquer método que use
+    print(p.nome)
+    p.nome = 'Renzo'
+    print(p.nome)
+    print(p.idade)
 
 """
 To import in Python Console:
